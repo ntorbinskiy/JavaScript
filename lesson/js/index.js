be482455db8aaa2ea280fd13ef06c414f6b3446c
Nikita <n.torbinskiy@gmail.com>
2020-03-15 13:04:17 +0200

    draft

--- lesson/js/index.js
+++ lesson/js/index.js
@@ -1,70 +1,73 @@
-let  money;
-let time;
-let money_1 = prompt("Ваш бюджет на месяц?");
-let time_1 = prompt("Введите дату в формате YYYY-MM-DD");
+// 'use strict';
 
-console.log(money_1);
-console.log(time_1);
+
+let money,time;
+
+function ShowFirstMassege() {
+    money = prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+    while(isNaN(money) || money=="" || money==null) {
+        money = prompt("Ваш бюджет на месяц?");
+    }
+} 
+ 
+ShowFirstMassege();
 
 let appData = {
-    budget: Number(money_1), // бюджет
-    timeData: time_1, // данные времени
+    budget: Number(money), // бюджет
+    timeData: time, // данные времени
     expenses: {
     }, // объект с обязательными расходами 
     optionalExpenses: {}, // объект с необязательными расходами
     income: [], // массив данных с доп. доходом
     savings: false, // свойство 
-    
+
 };
 
-
-/* for (let i = 0; i < 2; i++) {
-    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt ("Во сколько обойдется?", "");
-
-    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-
-        console.log ("done");
-
-        appData.expenses[a] = b;
-    } else {                            
-        console.log ("bad result");
-        i--;
+ function ShowSecondMassege() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt ("Во сколько обойдется?", "");
+    
+        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+    
+            console.log ("done");
+    
+            appData.expenses[a] = b;
+        } else {                            
+            console.log ("bad result");
+            i--;
+        }
     }
- 
-};*/
-/*  let i = 0;
-while(i<2) {
-let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-    b = prompt ("Во сколько обойдется?", "");
+ }
+ShowSecondMassege();
+console.log(appData);
 
-if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-    console.log ("done");
-    appData.expenses[a] = b;} 
-    else {                            
-    console.log ("bad result");
-    i--;
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget/30).toFixed();
+    alert("Ваш бюджет на день: " + appData.moneyPerDay + "грн");
+}
+detectDayBudget();
+
+
+function detectlevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log ("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log ("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log ("Это высокий уровень достатка!");
+    } else {
+        console.log ("Произошла ошибка");
     }
-    i++;
 }
- */  let i = 0; 
- do {
-    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt ("Во сколько обойдется?", "");
-if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-    console.log ("done");
-    appData.expenses[a] = b;
-} else {                            
-    console.log ("bad result");
-    i--;
+
+detectlevel();
+
+function chooseOptExpenses() {
+    
 }
-}
-while(i<2); 
-    console.log(appData);
-    appData.moneyPerDay = appData.budget / 30; 
- alert( "Ваш бюджет на день: " + appData.moneyPerDay + "грн"); 
-
- 
 
 
 
@@ -73,10 +76,11 @@ while(i<2);
 
 
 
- 
 
- 
- 
+
+
+
+
 
 
 
